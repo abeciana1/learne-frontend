fa0b7a901c9ab1025292b61475ac788582fac72a
Alex <alex.beciana@gmail.com>
2022-06-02 01:11:21 -0400

    Added ButtonGroup to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 //* import comps
 import CustomHead from '../components/core/CustomHead'
-import DefaultMargin from '../components/_layouts/DefaultMargin'
 import PageHeader from '../components/_headers/PageHeader'
+
+//* import layouts
+import DefaultMargin from '../components/_layouts/DefaultMargin'
 import TwoColumn from '../components/_layouts/TwoColumn'
+import ButtonGroup from '../components/_layouts/ButtonGroup'
 
 //* import libraries
 import Image from 'next/image'
@@ -24,6 +27,9 @@ const Home = () => {
               header="Be a better you"
               tagline="Create, enroll, or bring your own course. Be in command of your own growth and education!"
             />
+            <ButtonGroup>
+              
+            </ButtonGroup>
           </div>
           <Image
             src='/stack-of-books.png'
