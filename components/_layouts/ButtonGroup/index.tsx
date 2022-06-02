924beb92d6bff7e60f50b224b6528a4cf920a2c1
Alex <alex.beciana@gmail.com>
2022-06-02 00:59:50 -0400

    Created initial render of ButtonGroup layout comp

---
+++ components/_layouts/ButtonGroup/index.tsx
@@ -0,0 +1,11 @@
+
+
+const ButtonGroup = ({ children }: { children: any }) => {
+    return (
+        <div className="">
+            { children }
+        </div>
+    )
+}
+
+export default ButtonGroup
