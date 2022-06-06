83f6276269b22aff38c66edc4545379aa6470ce2
Alex <alex.beciana@gmail.com>
2022-06-06 18:17:08 -0400

    Created initial rendering of Course comp

---
+++ components/app/Course/index.tsx
@@ -0,0 +1,23 @@
+import cx from 'classnames'
+
+interface CourseI {
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+    href: string;
+}
+
+const Course = ({
+    name,
+    image,
+    description,
+    price,
+    href
+}: CourseI) => {
+    return (
+        <div>
+            
+        </div>
+    ) 
+}
